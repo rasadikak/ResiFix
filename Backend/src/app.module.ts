51e80472994869c5e25f56629554ab035c7fe247
kaushani rasadika <rasadikak@gmail.com>
2026-09-04 11:38:23 +0530

    initialized database

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    
+    ConfigModule.forRoot({
+      isGlobal: true, // no need to import ConfigModule in every other module
+    }),
+
+    // Connects to MongoDB using the URI from .env
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+
+    // Feature modules go here as you build them, e.g.:
+    // UsersModule,
+    // AuthModule,
+    // SectionsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
